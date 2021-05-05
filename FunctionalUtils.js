4b675eec76b8b717553fac2e4823d6e7def16d6b
juandeto <juandeto10@gmail.com>
2021-05-05 10:12:52 -0300

    pum

---
+++ FunctionalUtils.js
@@ -0,0 +1,14 @@
+
+
+//Function Composition: when the output of a function is other function
+
+const compose = (f, g) => x => f(g(x));
+
+const double = n => n * 2;
+const inc = n => n + 1;
+
+
+const transform = compose(double, inc);
+
+
+console.log(transform(3))
